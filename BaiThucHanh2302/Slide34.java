9fabd1d70e862dfbcbd324d29f9229004eb5b29a
KieuLong203 <kieuphilong24@gmail.com>
2023-02-23 15:27:00 +0700

    first commit

---
+++ BaiThucHanh2302/Slide34.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Slide34 {
+    public static void main(String[] args) {
+        int a,b,c;
+        float delta;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao a");
+        a = sc.nextInt();
+        System.out.println("Nhap vao b");
+        b = sc.nextInt();
+        System.out.println("Nhap vao c");
+        c = sc.nextInt();
+    }
+}
