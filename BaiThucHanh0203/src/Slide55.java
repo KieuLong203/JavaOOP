0da93394505dc77bbb2814fe234636fcfdfb65f6
KieuLong203 <kieuphilong24@gmail.com>
2023-03-02 15:23:11 +0700

    second commit

---
+++ BaiThucHanh0203/src/Slide55.java
@@ -0,0 +1,11 @@
+public class Slide55 {
+    public static void main(String[] args) {
+        int n = 0;
+        while (n>=0 && n<=200) {
+            if(n%5==0){
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
+}
